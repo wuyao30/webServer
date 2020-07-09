f94cd544cdca5c5527c1a8b55e5127e21c5534e6
wuyao20 <huyashangding@163.com>
2020-07-09 11:11:17 +0800

    resolve http method

--- app.js
+++ app.js
@@ -3,24 +3,34 @@ const queryString = require('querystring')
 
 const server = http.createServer((request, response) => {
     const method = request.method
+    const url = request.url
+    const path = url.split('?')[0]
+    const query = queryString.parse(url.split('?')[1])
+    
+    //set reponse data format
+    response.setHeader('Content-Type', 'application/json')
+    const resData = {
+        method,
+        url,
+        path,
+        query
+    }
     if(method === 'GET') {
-        const url = request.url
-        request.query = queryString.parse(url.split('?')[1])
         response.end(
-        JSON.stringify(request.query)
+            JSON.parse(resData)
         )
-    } else if (method === 'POST') {
-        const conttentType = request.headers['content-type']
-        console.log('request content type:', conttentType)
+    }
+
+    if(method === 'POST') {
         let postData = ''
         request.on('data', chunk => {
             postData += chunk.toString()
         })
         request.on('end', () => {
-            response.end(postData)
+            response.end(
+                JSON.parse(postData)
+            )
         })
-    } else {
-        response.end('')
     }
 })
 
