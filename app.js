40ba6f8ecbc69eb9eaa5193fcb8ac8b2561c56d5
wuyao20 <huyashangding@163.com>
2020-07-09 10:44:20 +0800

    queryString module

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-console.log(100)
-console.log(100)
-console.log(100)
-console.log(100)
-console.log(100)
-console.log(100)
-console.log(100)
-console.log(100)
+const http = require('http')
+const queryString = require('querystring')
+
+const server = http.createServer((request, response) => {
+    console.log('request url:', request.url)
+    const url = request.url
+    console.log('request method: ', request.method)
+    request.query = queryString.parse(url.split('?')[1])
+    console.log('query params: ', request.query)
+    response.end(
+        JSON.stringify(request.query)
+    )
+})
+
+server.listen(8888)
