88d0294c2926ef624217c7019b2597e57f4fceab
wuyao20 <huyashangding@163.com>
2020-07-09 11:01:58 +0800

    post method

--- app.js
+++ app.js
@@ -2,14 +2,26 @@ const http = require('http')
 const queryString = require('querystring')
 
 const server = http.createServer((request, response) => {
-    console.log('request url:', request.url)
-    const url = request.url
-    console.log('request method: ', request.method)
-    request.query = queryString.parse(url.split('?')[1])
-    console.log('query params: ', request.query)
-    response.end(
+    const method = request.method
+    if(method === 'GET') {
+        const url = request.url
+        request.query = queryString.parse(url.split('?')[1])
+        response.end(
         JSON.stringify(request.query)
-    )
+        )
+    } else if (method === 'POST') {
+        const conttentType = request.headers['content-type']
+        console.log('request content type:', conttentType)
+        let postData = ''
+        request.on('data', chunk => {
+            postData += chunk.toString()
+        })
+        request.on('end', () => {
+            response.end(postData)
+        })
+    } else {
+        response.end('')
+    }
 })
 
 server.listen(8888)
